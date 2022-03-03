03f1332baa7039fbc4591d3cafeb40c7e60d74de
GEAR-2012 <sandor.tudja@gmail.com>
2022-03-03 18:38:46 +0000

    new 404 page design

--- src/components/Pages/NotFoundPage.jsx
+++ src/components/Pages/NotFoundPage.jsx
@@ -1,10 +1,32 @@
 import React from "react";
+import { Box, Paper, Typography } from "@mui/material";
 
 const NotFoundPage = () => {
   return (
-    <>
-      <h1>Error! The page is not found.</h1>
-    </>
+    <Box>
+      <Box
+        sx={{
+          position: "absolute",
+          top: 0,
+          right: 0,
+          bottom: 0,
+          left: 0,
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Paper elevation={4} sx={{ display: "grid", justifyItems: "center", gap: 3, textAlign: "center", p: 3 }}>
+          <Typography variant="h3">404 Error</Typography>
+          <Typography variant="h6">
+            The page you <br /> requested was not found.
+          </Typography>
+          <Typography variant="h5">
+            Don't panic. <br /> Click on our Brand logo.
+          </Typography>
+        </Paper>
+      </Box>
+    </Box>
   );
 };
 
